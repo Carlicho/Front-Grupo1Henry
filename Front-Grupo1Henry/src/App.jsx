5231571a8e342a044020c85b57937bda60c14406
eott <elisolott@gmail.com>
2023-12-01 01:38:35 -0300

    fix(imports): arregla imports con problemas de mayusculas

--- Front-Grupo1Henry/src/App.jsx
+++ Front-Grupo1Henry/src/App.jsx
@@ -1,21 +1,18 @@
-import './App.css'
-import PathRoutes from './helpers/PathRoutes.Helpers.Js'
-import { Route, Routes } from 'react-router-dom'
-import About from './components/Views/About/About'
-import Header from './components/Header/Header'
-import Products from './components/products/products'
-import Help from './components/Help/Help'
-import Index from './components/Views/Index/Index'
-import Footer from './components/Footer/Footer'
+import { useState } from 'react';
+import PathRoutes from './helpers/PathRoutes.Helpers.Js';
+import { Route, Routes } from 'react-router-dom';
+
+import './App.css';
+import About from './components/Views/About/About';
+import Header from './components/Header/Header';
+import Products from './components/products/Products';
+import Help from './components/Help/Help';
+import Index from './components/Views/Index/Index';
+import Footer from './components/Footer/Footer';
 import axios from 'axios';
 
-import ProductDetail from './components/products/ProductDetailCard/ProductDetail'
-import UserSettings from './components/UserLogued/UserSettings'
-import { useState } from 'react'
-
-
-
-
+import ProductDetail from './components/products/ProductDetailCard/ProductDetail';
+import UserSettings from './components/UserLogued/UserSettings';
 
 function App() {
 
